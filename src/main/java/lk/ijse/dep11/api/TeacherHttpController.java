14b6eaa93ab4e9f0311ce0e9acd048450cde2b35
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-11-22 19:01:38 +0530

    commit 1

--- src/main/java/lk/ijse/dep11/api/TeacherHttpController.java
+++ src/main/java/lk/ijse/dep11/api/TeacherHttpController.java
@@ -1,33 +1,100 @@
 package lk.ijse.dep11.api;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import lk.ijse.dep11.to.TeacherTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.sql.*;
 
 @RestController
 @RequestMapping("/teachers")
 @CrossOrigin
 public class TeacherHttpController {
 
+    private final HikariDataSource pool;
+
+    public TeacherHttpController(){
+        HikariConfig config = new HikariConfig();
+        config.setUsername("root");
+        config.setPassword("1234");
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/dep11_practice_IMS");
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        config.addDataSourceProperty("maximumPoolSize", 10);
+        pool = new HikariDataSource(config);
+    }
+
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public TeacherTO addTeacher(@RequestBody TeacherTO teacherTO){}
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public TeacherTO addTeacher(@RequestBody TeacherTO teacherTO){
+        try(Connection connection=pool.getConnection()){
+            PreparedStatement stm = connection.prepareStatement("INSERT INTO teacher (id,name,contact) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
+            stm.setString(1,teacherTO.getName());
+            stm.setString(2, teacherTO.getContact());
+            stm.executeUpdate();
+            ResultSet generatedKeys = stm.getGeneratedKeys();
+            generatedKeys.next();
+            int id = generatedKeys.getInt(1);
+            teacherTO.setId(id);
+            return teacherTO;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PatchMapping("/{id}")
-    public void updateTeacher(@RequestBody TeacherTO teacherTO){}
+    @PatchMapping(value = "/{id}",consumes = "application/json")
+    public void updateTeacher(@PathVariable int id,
+                              @RequestBody TeacherTO teacherTO){
+        try(Connection connection= pool.getConnection()) {
+            PreparedStatement existSTM = connection.prepareStatement("SELECT * FROM teacher WHERE id=?");
+            existSTM.setInt(1,id);
+
+            if(!existSTM.executeQuery().next()){
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Teacher Not found")
+            }
+            PreparedStatement stm = connection.prepareStatement("UPDATE teacher SET name=? contact=? WHERE id=?");
+            stm.setString(1,teacherTO.getName());
+            stm.setString(2, teacherTO.getContact());
+            stm.setInt(3,id);
+
+            stm.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void deleteTeacher(){}
+    public void deleteTeacher(@PathVariable int id){
+        try(Connection connection= pool.getConnection()){
+            PreparedStatement existSTM = connection.prepareStatement("SELECT * FROM teacher WHERE id=?");
+            existSTM.setInt(1,id);
 
-    @GetMapping("/{id}")
+            if(!existSTM.executeQuery().next()){
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Teacher Not found")
+            }
+
+            connection.prepareStatement("DELETE FROM ")
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping(value = "/{id}",produces = "application/json")
     public TeacherTO getTeacher(){
+        System.out.println("get");
         return null;
     }
 
-    @GetMapping
+    @GetMapping(produces = "application/json")
     public TeacherTO getALlTeacher(){
+        System.out.println("getAll");
         return null;
     }
 }
